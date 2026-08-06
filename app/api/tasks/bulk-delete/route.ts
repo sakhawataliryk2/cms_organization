f6cdf19b971bbf4c2100b2e729974f03c9a19cec
Nouman Tariq <nt50616849@gmail.com>
2026-08-06 10:52:45 +0500

    Job Seekers Deletetion Issues Fixed - Next.js Proxy Missing

---
+++ app/api/tasks/bulk-delete/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+import { proxyBulkDelete } from "@/lib/proxyBulkDelete";
+
+export async function POST(request: NextRequest) {
+  return proxyBulkDelete(request, "tasks");
+}
